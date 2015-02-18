f59e17b5c2e2e80beaaec06682263667ee55a308
Harry Harpham <hwh33@cornell.edu>
2015-02-18 07:10:19 -0500

    Add simple HTTP server

---
+++ server.go
@@ -0,0 +1,29 @@
+""" This is the simple HTTP server from the 'Tour of Go' tutorial.
+
+It can be found here: https://tour.golang.org/methods/13
+
+"""
+
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+type Hello struct{}
+
+func (h Hello) ServeHTTP(
+	w http.ResponseWriter,
+	r *http.Request) {
+	fmt.Fprint(w, "Hello!")
+}
+
+func main() {
+	var h Hello
+	err := http.ListenAndServe("localhost:4000", h)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
