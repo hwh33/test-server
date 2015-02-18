db61d1959ceeff6f164ef4c9b66416f049c0f0d3
Harry Harpham <hwh33@cornell.edu>
2015-02-18 08:28:11 -0500

    Add custom welcome message.

--- server.go
+++ server.go
@@ -1,4 +1,4 @@
-/* This is the simple HTTP server from the 'Tour of Go' tutorial.
+/* This is essentially the simple HTTP server from the 'Tour of Go' tutorial.
  *
  * It can be found here: https://tour.golang.org/methods/13
  */
@@ -7,19 +7,30 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 )
+
+// Read the welcome message from file and print it.
+var welcome_msg_file = "WelcomeMessage.txt"
+var welcome_msg_buffer, err = ioutil.ReadFile(welcome_msg_file)
 
 type Hello struct{}
 
 func (h Hello) ServeHTTP(
 	w http.ResponseWriter,
 	r *http.Request) {
-	fmt.Fprint(w, "Hello!")
+
+	fmt.Fprint(w, string(welcome_msg_buffer))
 }
 
 func main() {
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
 	var h Hello
 	err := http.ListenAndServe(":80", h)
 	if err != nil {
