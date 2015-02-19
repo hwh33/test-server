07098f4de240d1dbe78e1216412193849c7c2d80
Harry Harpham <hwh33@cornell.edu>
2015-02-19 08:23:53 -0500

    Simplify ssl_server.go, remove server.go

--- ssl_server.go
+++ ssl_server.go
@@ -1,10 +1,7 @@
 package main
 
 import (
-	"crypto/tls"
-	"crypto/x509"
 	"fmt"
-	"io/ioutil"
 	"net/http"
 	"os"
 )
@@ -17,32 +14,18 @@ func (h SSLHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	const sslDir = "ssl-cert" + string(os.PathSeparator)
-	const caCert = sslDir + "ca.pem"
 	const certChainFile = sslDir + "cert_chain.crt"
 	const keyFile = sslDir + "decrypted.ssl.key"
 
 	var handler SSLHandler
 
-	rootPEM, err := ioutil.ReadFile(caCert)
-	if err != nil {
-		panic("Failed to read certificate file: " + err.Error())
-	}
-
-	roots := x509.NewCertPool()
-	ok := roots.AppendCertsFromPEM([]byte(rootPEM))
-	if !ok {
-		panic("Failed to parse root certificate")
-	}
-
-	config := &tls.Config{RootCAs: roots}
 	server := http.Server{
-		Addr:      ":443",
+		Addr:      ":http",
 		Handler:   handler,
-		TLSConfig: config,
 	}
 
-	err = server.ListenAndServeTLS(certChainFile, keyFile)
-	// err = server.ListenAndServe()
+	// err := server.ListenAndServeTLS(certChainFile, keyFile)
+	err := server.ListenAndServe()
 	if err != nil {
 		panic("Server failure: " + err.Error())
 	}
